fb1617e17e251446bdead1df4d76711478ac5223
KrishnaKRGit <krishnakrdms@gmail.com>
2017-03-23 06:51:30 -0400

    Just testing a commit from intelliJ

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("what up");
-        System.out.println("ayyyy nothing much just testing a push to git"); 
+        System.out.println("ayyyy nothing much just testing a push to git");
+        System.out.println("Did the push work?");
     }
 }
