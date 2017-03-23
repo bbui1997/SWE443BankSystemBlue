60e40d5f28b074638e7069dad4d1312550325f5e
salonikab <salonikabose@gmail.com>
2017-03-23 09:38:36 -0400

    "Added Hello from Salonika"

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("ayyyy nothing much just testing a push to git");
         System.out.println("Did the push work?");
         System.out.println("Hello from Heymond");
+        System.out.println("Hello from Salonika");
     }
 }
