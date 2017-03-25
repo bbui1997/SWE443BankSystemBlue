141a7eebc816f5347f1b5c88e9a4c40a516aeac2
salonikab <salonikabose@gmail.com>
2017-03-25 14:45:38 -0400

    "Updated Account, BlueBank, and CreateAccountTest. Created input files for Scenarios from Salonika"

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,10 +1,13 @@
+import java.util.Date;
+
 /**
- * Created by Salonika on 3/25/17. Started working at 11:35 am - 12:51 pm
+ * Created by Salonika on 3/25/17. Started working at 11:35 am - 12:51 pm; 1:54pm - 2:43 pm
  */
 public class Account {
     String name;
     int ssn;
-    int dob;
+    //Date should be an actual Date obj but just for simplicity I am keeping it a String for now
+    String dob;
     String username;
     String password;
     double initialAmount;
@@ -29,11 +32,12 @@ public class Account {
         this.ssn = ssn;
     }
 
-    public int getDob() {
+    public String getDob() {
+
         return dob;
     }
 
-    public void setDob(int dob) {
+    public void setDob(String dob) {
         this.dob = dob;
     }
 
@@ -50,15 +54,30 @@ public class Account {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        if(password.length() >= 4){
+            this.password = password;
+        }
+        else {
+            System.out.println("The password you've entered is too short. Please enter a different password that is at least four (4) characters.");
+            this.password = "";
+        }
+
     }
+
 
     public double getInitialAmount() {
         return initialAmount;
     }
 
     public void setInitialAmount(double initialAmount) {
-        this.initialAmount = initialAmount;
+        if(initialAmount > 0.0) {
+            this.initialAmount = initialAmount;
+        }
+        else {
+            System.out.println("Please do not enter negative number. Please enter a positve initial amount to deposit.");
+            this.initialAmount = 0.00;
+        }
+
     }
 
 
