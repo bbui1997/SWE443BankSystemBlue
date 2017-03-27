903b569c97e5429d81fa3997ce55cbc91b13d603
saraborghei <sara.borghei@gmail.com>
2017-03-27 12:26:49 -0400

    Added WELCOME_SCREEN Scenarios and task, added LOG_IN Scenarios and task Added PersistenceLayer task

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,12 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("what up");
+        System.out.println("what's up");
         System.out.println("ayyyy nothing much just testing a push to git");
         System.out.println("Did the push work?");
         System.out.println("Hello from Heymond");
         System.out.println("Hello from Salonika!");
         System.out.println("Hello from Ermal!");
         System.out.println("Hello from Ashmita");
-
+        System.out.println("Hello from Sara");
     }
 }
