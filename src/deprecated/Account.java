92f352f798c99675bc9170994d1de2ac024c2706
bbui1997 <bbui1997@gmail.com>
2017-04-04 00:05:17 -0400

    Reorganized kanban folders. Made capitalization consistent. Moved guideline type files outside of done folder. Added burndown folder and chart

--- src/deprecated/Account.java
+++ src/deprecated/Account.java
@@ -1,7 +1,3 @@
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
